7cdb34bbaa6640f1b7f2513cb62b5e3b804b9dd2
AvinashYadav121 <todaynewid123@gmail.com>
2026-02-17 19:39:56 +0530

    Added AI username detection route

--- services/image_service.py
+++ services/image_service.py
@@ -1,4 +1,12 @@
+# from image_ai.predict_image import get_image_score
+
+# def analyze_profile_image(url):
+#     return get_image_score(url)
 from image_ai.predict_image import get_image_score
 
-def analyze_profile_image(url):
-    return get_image_score(url)
+def analyze_image(url):
+    try:
+        score = get_image_score(url)
+        return score
+    except:
+        return 0.0
